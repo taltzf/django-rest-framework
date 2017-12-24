43effa262be7abacfbe9def9f85f0bb9b7000089
TalTzf <tal.tzf@gmail.com>
2017-12-24 15:13:43 +0200

    Quickstart - finished

---
+++ tutorial/web_pages/Quickstart - Django REST framework_files/sidebar_include.js
@@ -0,0 +1,16 @@
+var include = '';
+
+
+
+include += '<a href="https://rollbar.com/">';
+include += '<img width="130px" src="https://fund-rest-framework.s3.amazonaws.com/rollbar2.png"></img>';
+include += '</a>';
+
+
+include += '<p><a class="promo" href="https://rollbar.com/">Real-time error monitoring, alerting, and analytics for developers. </a></p>';
+
+
+
+include += '<p><a href="https://fund.django-rest-framework.org/topics/funding/">Fund Django REST framework</a></p>'
+
+document.getElementById('sidebarInclude').innerHTML = include;
