587bb78dfae7298cdd911be62cd485a00689a6f2
TalTzf <tal.tzf@gmail.com>
2018-01-28 00:10:11 +0200

    Tutorial 2: Requests and Responses

---
+++ tutorial/web_pages/2 - Requests and responses - Django REST framework_files/sidebar_include.js
@@ -0,0 +1,16 @@
+var include = '';
+
+
+
+include += '<a href="https://about.holvi.com/careers/">';
+include += '<img width="130px" src="https://fund-rest-framework.s3.amazonaws.com/holvi-logo-green-small.jpg"></img>';
+include += '</a>';
+
+
+include += '<p><a class="promo" href="https://about.holvi.com/careers/">Join us in building a new type of banking service for European entrepreneurs and freelancers.</a></p>';
+
+
+
+include += '<p><a href="https://fund.django-rest-framework.org/topics/funding/">Fund Django REST framework</a></p>'
+
+document.getElementById('sidebarInclude').innerHTML = include;
