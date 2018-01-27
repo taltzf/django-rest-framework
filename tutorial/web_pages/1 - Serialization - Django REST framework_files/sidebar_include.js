0c24c73ea24cdd6d9c3ab608373918b48949212a
TalTzf <tal.tzf@gmail.com>
2018-01-27 23:35:14 +0200

    tutorial01-serialization

---
+++ tutorial/web_pages/1 - Serialization - Django REST framework_files/sidebar_include.js
@@ -0,0 +1,16 @@
+var include = '';
+
+
+
+include += '<a href="http://www.ismltd.com">';
+include += '<img width="130px" src="https://fund-rest-framework.s3.amazonaws.com/ism_games.png"></img>';
+include += '</a>';
+
+
+include += '<p><a class="promo" href="http://www.ismltd.com">ISM Fantasy Games - We design, develop and operate large scale fantasy sports games.</a></p>';
+
+
+
+include += '<p><a href="https://fund.django-rest-framework.org/topics/funding/">Fund Django REST framework</a></p>'
+
+document.getElementById('sidebarInclude').innerHTML = include;
