b9a0755dbc2a3101d82937f405c2589f8c42ab83
TalTzf <tal.tzf@gmail.com>
2018-01-28 12:57:37 +0200

    Tutorial 4: Authentication & Permissions

---
+++ tutorial/web_pages/4 - Authentication and permissions - Django REST framework_files/sidebar_include.js
@@ -0,0 +1,16 @@
+var include = '';
+
+
+
+include += '<a href="https://www.isotoma.com/jobs/">';
+include += '<img width="130px" src="https://fund-rest-framework.s3.amazonaws.com/isotoma.png"></img>';
+include += '</a>';
+
+
+include += '<p><a class="promo" href="https://www.isotoma.com/jobs/">Passionate about the web and Django? We’re keen to find the right people to join our close knit, friendly and enthusiastic team.</a></p>';
+
+
+
+include += '<p><a href="https://fund.django-rest-framework.org/topics/funding/">Fund Django REST framework</a></p>'
+
+document.getElementById('sidebarInclude').innerHTML = include;
