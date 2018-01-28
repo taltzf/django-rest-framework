3d65b173d444f8922cc17992c4d07848ee1807b0
TalTzf <tal.tzf@gmail.com>
2018-01-28 17:28:30 +0200

    Tutorial 5: Relationships & Hyperlinked APIs

---
+++ tutorial/web_pages/5 - Relationships and hyperlinked APIs - Django REST framework_files/sidebar_include.js
@@ -0,0 +1,16 @@
+var include = '';
+
+
+
+include += '<a href="http://www.zupr.io">';
+include += '<img width="130px" src="https://fund-rest-framework.s3.amazonaws.com/zupr.png"></img>';
+include += '</a>';
+
+
+include += '<p><a class="promo" href="http://www.zupr.io">Zupr - the online link to offline sales.</a></p>';
+
+
+
+include += '<p><a href="https://fund.django-rest-framework.org/topics/funding/">Fund Django REST framework</a></p>'
+
+document.getElementById('sidebarInclude').innerHTML = include;
