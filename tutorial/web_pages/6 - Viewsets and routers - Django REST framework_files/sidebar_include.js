91923f0df27d9f5b14b8aeecdf8c4fc6b58324d0
TalTzf <tal.tzf@gmail.com>
2018-01-30 17:50:03 +0200

    Tutorial 6: ViewSets & Routers

---
+++ tutorial/web_pages/6 - Viewsets and routers - Django REST framework_files/sidebar_include.js
@@ -0,0 +1,16 @@
+var include = '';
+
+
+
+include += '<a href="https://makespace.com">';
+include += '<img width="130px" src="https://fund-rest-framework.s3.amazonaws.com/makespace_logo_130.png"></img>';
+include += '</a>';
+
+
+include += '<p><a class="promo" href="https://makespace.com">Never visit a storage unit again. We pick up, store, and bring back your stuff – all for less than self storage.</a></p>';
+
+
+
+include += '<p><a href="https://fund.django-rest-framework.org/topics/funding/">Fund Django REST framework</a></p>'
+
+document.getElementById('sidebarInclude').innerHTML = include;
