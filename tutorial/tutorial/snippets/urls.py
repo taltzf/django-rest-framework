231772d3e3b065e7ec74a2fb66fba1989e4a2f48
TalTzf <tal.tzf@gmail.com>
2018-01-30 18:39:52 +0200

    Tutorial 7: Schemas & client libraries

--- tutorial/tutorial/snippets/urls.py
+++ tutorial/tutorial/snippets/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from rest_framework.schemas import get_schema_view
+
+
+schema_view = get_schema_view(title='Pastebin API')
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -9,6 +13,7 @@ router.register(r'users', views.UserViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^', include(router.urls))
 ]
 
---
+++ tutorial/web_pages/7 - Schemas and client libraries - Django REST framework_files/sidebar_include.js
@@ -0,0 +1,16 @@
+var include = '';
+
+
+
+include += '<a href="https://www.futrli.com/careers/">';
+include += '<img width="130px" src="https://fund-rest-framework.s3.amazonaws.com/futrli.png"></img>';
+include += '</a>';
+
+
+include += '<p><a class="promo" href="https://www.futrli.com/careers/">FUTRLI: Award-winning business forecasting, reporting and alerts.</a></p>';
+
+
+
+include += '<p><a href="https://fund.django-rest-framework.org/topics/funding/">Fund Django REST framework</a></p>'
+
+document.getElementById('sidebarInclude').innerHTML = include;
